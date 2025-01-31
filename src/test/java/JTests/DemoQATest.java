00daf4080971ad0998b2aa00515d9c1bc2197c27
mixernsk <mmmmx1@ya.ru>
2025-01-31 09:26:49 +0700

    added soft asserts and file object

--- src/test/java/JTests/DemoQATest.java
+++ src/test/java/JTests/DemoQATest.java
@@ -2,9 +2,12 @@ package JTests;
 /*
 Проверка корректной обработки формы регистрации
  */
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import pages.FormPage;
+
+import java.io.File;
 import java.util.Map;
 
 public class DemoQATest extends BaseTest {
@@ -23,8 +26,8 @@ public class DemoQATest extends BaseTest {
         String bYear = "1985";
         String checkDate = "23 September,1985"; // Дата в формате необходимом для проверки в результатах
         String subjects = "Maths";
-        String picPath = "C:\\downloads\\testpic.jpg";
-        String picName = "testpic.jpg"; // Имя файла изображения для проверки в результатах
+        File picture = new File("src/testpic.jpg");
+        String picName = picture.getName(); // Имя файла изображения для проверки в результатах
         String address = "Los Angeles, Rodeo Dv., 25";
         String state = "Haryana";
         String city = "Panipat";
@@ -53,7 +56,7 @@ public class DemoQATest extends BaseTest {
         formPage.checkReading();
         formPage.checkSports();
 
-        formPage.selectPictureFile(picPath);
+        formPage.selectPictureFile(picture.getAbsolutePath());
 
         formPage.inputAddress(address);
 
@@ -66,20 +69,21 @@ public class DemoQATest extends BaseTest {
 
         assertTrue(formPage.resultsTableDisplayed());
         Map<String, String> results = formPage.getResults();
-
-        assertTrue(results.get("fullName").contains(firstName), "Name assertion error");
-        assertTrue(results.get("fullName").contains(lastName), "Last name assertion error");
-        assertTrue(results.get("email").contains(email), "Email assertion error");
-        assertTrue(results.get("gender").contains("Male"), "Gender assertion error");
-        assertTrue(results.get("mobile").contains(mobile), "Mobile assertion error");
-        assertTrue(results.get("birthDate").contains(checkDate), "Date assertion error" + results.get("birthDate"));
-        assertTrue(results.get("subjects").contains(subjects), "Subjects assertions error");
-        assertTrue(results.get("hobbies").contains("Sports"), "Sports assertion error");
-        assertTrue(results.get("hobbies").contains("Reading"), "Reading assertion error");
-        assertTrue(results.get("hobbies").contains("Music"), "Music");
-        assertTrue(results.get("picture").contains(picName), "Picname");
-        assertTrue(results.get("address").contains(address), "Address");
-        assertTrue(results.get("stateCity").contains(state), "State");
-        assertTrue(results.get("stateCity").contains(city), "City");
+        assertAll(
+            () -> assertTrue(results.get("fullName").contains(firstName), "Name assertion error"),
+            () -> assertTrue(results.get("fullName").contains(lastName), "Last name assertion error"),
+            () -> assertTrue(results.get("email").contains(email), "Email assertion error"),
+            () -> assertTrue(results.get("gender").contains("Male"), "Gender assertion error"),
+            () -> assertTrue(results.get("mobile").contains(mobile), "Mobile assertion error"),
+            () -> assertTrue(results.get("birthDate").contains(checkDate), "Date assertion error" + results.get("birthDate")),
+            () -> assertTrue(results.get("subjects").contains(subjects), "Subjects assertions error"),
+            () -> assertTrue(results.get("hobbies").contains("Sports"), "Sports assertion error"),
+            () -> assertTrue(results.get("hobbies").contains("Reading"), "Reading assertion error"),
+            () -> assertTrue(results.get("hobbies").contains("Music"), "Music"),
+            () -> assertTrue(results.get("picture").contains(picName), "Picname"),
+            () -> assertTrue(results.get("address").contains(address), "Address"),
+            () -> assertTrue(results.get("stateCity").contains(state), "State"),
+            () -> assertTrue(results.get("stateCity").contains(city), "City")
+        );
     }
 }
